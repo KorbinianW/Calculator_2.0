7ae3a227e9b67c3f46e683e5d5ffee73e26e03a8
KorbinianW <129525284+KorbinianW@users.noreply.github.com>
2024-01-13 20:03:43 +0100

    added GUI

--- src/main/java/com/example/calculator/HelloController.java
+++ src/main/java/com/example/calculator/HelloController.java
@@ -1,14 +1,121 @@
 package com.example.calculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class HelloController {
+
+    String calculation = "";
     @FXML
-    private Label welcomeText;
+    private Label Display;
+    @FXML
+    private Button N0;
+    @FXML
+    private Button N1;
+    @FXML
+    private Button N2;
+    @FXML
+    private Button N3;
+    @FXML
+    private Button N4;
+    @FXML
+    private Button N5;
+    @FXML
+    private Button N6;
+    @FXML
+    private Button N7;
+    @FXML
+    private Button N8;
+    @FXML
+    private Button N9;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onN0Clicked() {
+        calculation = calculation + "0";
+        Display.setText(calculation);
     }
+
+    @FXML
+    protected void onN1Clicked() {
+        calculation = calculation + "1";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onN2Clicked() {
+        calculation = calculation + "2";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onN3Clicked() {
+        calculation = calculation + "3";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onN4Clicked() {
+        calculation = calculation + "4";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onN5Clicked() {
+        calculation = calculation + "5";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onN6Clicked() {
+        calculation = calculation + "6";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onN7Clicked() {
+        calculation = calculation + "7";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onN8Clicked() {
+        calculation = calculation + "8";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onN9Clicked() {
+        calculation = calculation + "9";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onPlusClicked() {
+        calculation = calculation + "+";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onMinusClicked() {
+        calculation = calculation + "-";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onMultipleClicked() {
+        calculation = calculation + "*";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onDivideClicked() {
+        calculation = calculation + "/";
+        Display.setText(calculation);
+    }
+    @FXML
+    protected void onDeleteClicked() {
+
+        Display.setText(calculation);
+
+    }
+    @FXML
+    protected void onEqualsClicked() {
+        char[] result = calculation.toCharArray();
+        int i = 0;
+        while (i <= calculation.length()){
+            
+        };
+    }
+
 }
